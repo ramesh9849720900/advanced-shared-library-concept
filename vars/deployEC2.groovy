73cd4316ed1f90d1edd48b04511e00dc68c33d31
ramesh9849720900 <114875739+ramesh9849720900@users.noreply.github.com>
2025-05-21 11:09:25 +0530

    Update deployEC2.groovy

--- vars/deployEC2.groovy
+++ vars/deployEC2.groovy
@@ -1,1 +1,5 @@
-#Write your code here...
+def call(Map config) {
+    echo "Deploying to EC2 instance: ${config.instanceId}"
+    sh "scp ${config.artifact} ec2-user@${config.instanceIp}:${config.targetDir}"
+    sh "ssh ec2-user@${config.instanceIp} 'bash ${config.deployScript}'"
+}
