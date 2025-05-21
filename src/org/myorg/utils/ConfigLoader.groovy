9338054754a62ae41ff7307d27aaaaa87554dd79
ramesh9849720900 <114875739+ramesh9849720900@users.noreply.github.com>
2025-05-21 11:11:26 +0530

    Update ConfigLoader.groovy

--- src/org/myorg/utils/ConfigLoader.groovy
+++ src/org/myorg/utils/ConfigLoader.groovy
@@ -1,1 +1,14 @@
-#Write your code here...
+package org.myorg.utils
+
+class ConfigLoader {
+    static Map load(String team) {
+        switch(team) {
+            case "teamA":
+                return [clusterName: "cluster-a", image: "team-a/app:latest"]
+            case "teamB":
+                return [clusterName: "cluster-b", image: "team-b/app:latest"]
+            default:
+                return [:]
+        }
+    }
+}
