3a37cdd381178dc3ce12f4c5dd7fa88bee2beda7
ramesh9849720900 <114875739+ramesh9849720900@users.noreply.github.com>
2025-05-21 22:55:20 +0530

    Update deployKubernetes.groovy

--- vars/deployKubernetes.groovy
+++ vars/deployKubernetes.groovy
@@ -1,5 +1,11 @@
 def call(Map config) {
     echo "Deploying to Kubernetes: ${config.clusterName}"
+
+    // Extract the YAML file from the shared library
+    def yamlContent = libraryResource(config.manifest)
+    def manifestFile = config.manifest.tokenize('/')[-1] // Extract filename only
+    writeFile file: manifestFile, text: yamlContent
+
     sh "kubectl config use-context ${config.context}"
-    sh "kubectl apply -f ${config.manifest}"
+    sh "kubectl apply -f ${manifestFile}"
 }
