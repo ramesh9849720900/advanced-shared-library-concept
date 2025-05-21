bc46b2c78f1ada962debba888c5483e0aaa84ab2
ramesh9849720900 <114875739+ramesh9849720900@users.noreply.github.com>
2025-05-21 11:08:44 +0530

    Update deployDocker.groovy

--- vars/deployDocker.groovy
+++ vars/deployDocker.groovy
@@ -1,1 +1,5 @@
-#Write your code here
+def call(Map config) {
+    echo "Deploying Docker container: ${config.image}"
+    sh "docker pull ${config.image}"
+    sh "docker run -d -p ${config.port}:${config.port} ${config.image}"
+}
