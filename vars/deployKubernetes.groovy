2d1cb810f0b34540aa05c41bf6b9fd520480e175
ramesh9849720900 <114875739+ramesh9849720900@users.noreply.github.com>
2025-05-21 11:08:17 +0530

    Update deployKubernetes.groovy

--- vars/deployKubernetes.groovy
+++ vars/deployKubernetes.groovy
@@ -1,1 +1,5 @@
-#Write your code 
+def call(Map config) {
+    echo "Deploying to Kubernetes: ${config.clusterName}"
+    sh "kubectl config use-context ${config.context}"
+    sh "kubectl apply -f ${config.manifest}"
+}
